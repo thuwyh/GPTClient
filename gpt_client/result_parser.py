9f3f8e720e41a106fcf17fd54f4bc05c2f08679b
Yuanhao Wu <wuyhthu@gmail.com>
2024-03-21 13:34:41 +0800

    add json parser

--- gpt_client/result_parser.py
+++ gpt_client/result_parser.py
@@ -1,3 +1,4 @@
+import json
 from typing import List
 from openai.types.chat import ChatCompletion
 
@@ -6,6 +7,15 @@ def parse_results_as_strings(results: List[ChatCompletion], default_value=''):
     for r in results:
         try:
             ret.append(r.choices[0].message.content)
+        except:
+            ret.append(default_value)
+    return ret
+
+def parse_results_as_jsons(results: List[ChatCompletion], default_value=None):
+    ret = []
+    for r in results:
+        try:
+            ret.append(json.loads(r.choices[0].message.content))
         except:
             ret.append(default_value)
     return ret
